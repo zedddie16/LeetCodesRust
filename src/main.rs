8f5bb2e839f27cc58673d363ba223f5f6eec622b
Matt <zedddiezxc@gmail.com>
2024-08-20 09:45:35 +0100

    two sum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+
+
 fn main() {
-    
+    let nums = vec![3,2,4];
+    let target = 6;
+    println!("{:?}", two_sum(nums, target));
+}
+fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    for item in 0..nums.len() - 1{
+        //println!("item = {}", item);
+        for i in item + 1..nums.len(){
+            //println!("i = {}", i);
+            if nums[item] + nums[i] == target{
+                let ans = vec![item as i32, i as i32]; 
+                return ans
+            }
+            //println!("{} + {} != {}",nums[item], nums[i], target);
+        }
+    }
+    return vec![0];
 }
