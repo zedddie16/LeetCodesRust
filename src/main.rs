6465e999cd8bb4b2c88b5f5395f48033bb1b5d7f
Matt <zedddiezxc@gmail.com>
2024-08-23 17:12:39 +0100

    find-complement

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
-
-
-fn main() {
-    let nums = vec![3,2,4];
-    let target = 6;
-    println!("{:?}", two_sum(nums, target));
+fn main(){
+    let num = 5;
+    find_complement(num)
 }
-fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    for item in 0..nums.len() - 1{
-        //println!("item = {}", item);
-        for i in item + 1..nums.len(){
-            //println!("i = {}", i);
-            if nums[item] + nums[i] == target{
-                let ans = vec![item as i32, i as i32]; 
-                return ans
-            }
-            //println!("{} + {} != {}",nums[item], nums[i], target);
+pub fn find_complement(num: i32) {
+    let binary = format!("{:b}", num);
+    let mut vec_binary: Vec<char> = binary.chars().collect::<Vec<_>>();
+    let mut ans:String = "".to_string();
+    println!("{:?}", vec_binary);
+        for i in 0..vec_binary.len(){
+            if vec_binary[i] == '0'{
+                ans = ans + "1";       
+        }else {
+            ans = ans + "0";
         }
     }
-    return vec![0];
+        println!("{:?}", ans );
+        let ans2 = binary_string_to_decimal(&ans).unwrap();
+        println!("{}", ans2)
+}
+fn binary_string_to_decimal(binary_string: &str) -> Result<i32, String> {
+    let mut decimal_value = 0;
+    let base = 2;
+
+    for (i, char) in binary_string.chars().rev().enumerate() {
+        let digit = char.to_digit(base).unwrap();
+        decimal_value += digit * base.pow(i as u32);
+    }
+
+    Ok(decimal_value as i32)
 }
